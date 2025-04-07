233a6942715ba0bcc1b8de9c9ddc513fe4077b4c
kavin-mohanadas-ics4u1-02-25 <kavin.mohanadas@student.tdsb.on.ca>
2025-04-07 20:50:17 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,52 @@ public class PracticeProblem {
 
 	}
 
+	public static void selectionSortName(String[] names, int[] ages) {
+		int n = names.length;
+		for (int i = 0; i < n -1; i++) {
+			int minIndex = i;
+
+			for (int j = i + 1; j < n; j++) {
+				if (names[j].compareToIgnoreCase(names[minIndex]) < 0) {
+					minIndex = j;
+				}
+			}
+			if (minIndex != i) {
+				String tempName = names[i];
+				names[i] = names[minIndex];
+				names[minIndex] = tempName;
+
+				int tempAge = ages[i];
+				ages[i] = ages[minIndex];
+				ages[minIndex] = tempAge
+;			}
+
+		}
+	}
+
+	public static void selectionSortAge(String[] names, int[] ages) {
+		int n = ages.length;
+		for(int i = 0; i < n - 1; i++) {
+			int minIndex = i;
+
+			for(int j = i+1; j<n; j++) {
+				if (ages[j] < ages[minIndex]) {
+					minIndex = j;
+				}
+			}
+
+			if (minIndex != i) {
+				int tempAge = ages[i];
+				ages[i] = ages[minIndex];
+				ages[minIndex] = tempAge;
+
+				String tempName = names[i];
+				names[i] = names[minIndex];
+				names[minIndex] = tempName;
+			}
+		}
+	}
+
 	
 
 }
